c497a0663e7574c5fd4e600c9dca7cf4ceebf7d2
payamaya <paulyashou81@gmail.com>
2025-07-03 01:32:52 +0200

    feat: add server proxy configuration for API requests in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080', // your backend server
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''), // optional
+      },
+    },
+  },
   plugins: [react()],
 })
