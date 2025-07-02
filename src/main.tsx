82bc554f0e8db39fceeae19c3f666cb7b60801fa
payamaya <paulyashou81@gmail.com>
2025-07-02 19:09:30 +0200

    feat: add .env file for environment variables and update .gitignore to include it

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 // main.tsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import App from './App'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
