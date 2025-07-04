01d671bfd994124dcd7a474d93e71a2a212caf48
payamaya <paulyashou81@gmail.com>
2025-07-04 16:53:19 +0200

    fix: change API request method from GET to POST in AIChat component

--- src/components/AIChat.tsx
+++ src/components/AIChat.tsx
@@ -148,7 +148,7 @@ const AIChat = ({ googleUser }: AIChatProps) => {
       const sanitizedInput = input.replace(/<[^>]*>?/gm, '')
 
       const res = await fetch('api/chat', {
-        method: 'GET',
+        method: 'POST',
         signal: abortControllerRef.current.signal,
         headers: {
           'Content-Type': 'application/json',
