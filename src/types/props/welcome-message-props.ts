729d409b9732d10a627b92418cf76c4e284a9329
payamaya <paulyashou81@gmail.com>
2025-07-23 15:43:38 +0200

    refactor: update WelcomeMessageProps to use GoogleUser type instead of AIChatProps

--- src/types/props/welcome-message-props.ts
+++ src/types/props/welcome-message-props.ts
@@ -1,9 +1,9 @@
-import type { AIChatProps } from './ai-chat-props'
 import type { UserProfile } from '../user/user-profile'
+import type { GoogleUser } from '../user/google-user'
 
 export interface WelcomeMessageProps {
   userProfile: UserProfile
-  googleUser?: AIChatProps['googleUser']
+  googleUser?: GoogleUser
   onEditProfile: (show: boolean) => void
   onAvatarChange: (newAvatarFile: File | null) => void
   editableAvatarUrl?: string
